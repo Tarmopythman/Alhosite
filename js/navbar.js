9c3d9bd22f1bda16d000d65ffe9aff067b8d309b
NicodemusLocal <niilo.nevala@gmail.com>
2024-01-23 12:21:13 +0200

    Reorganizing

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,8 @@
 document.getElementById('navbar-placeholder').innerHTML = `
     <nav>
         <ul>
-        <li><a href="/build/gov/index.html">Hallituksen asiakirjat</a></li>
+        <li><a href="/build/gov/index.html">Hallinnon asiakirjat</a></li>
         <li><a href="/build/law/index.html">Lainsäädäntö</a></li>
-        <li><a href="/build/par/index.html">Eduskunnan asiakirjat</a></li>
         <li><a href="/build/new/index.html">Uutiset ja tiedotteet</a></li>
         <li><a href="/build/doc/index.html">Asiakirjaindeksi</a></li>
         </ul>
